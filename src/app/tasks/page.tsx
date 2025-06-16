f1096913f443a77d95f4c97984afb00e3c377c09
vacho <vachisinio@gmail.com>
2025-06-16 16:08:01 -0400

    api: add get, post services

---
+++ src/app/tasks/page.tsx
@@ -0,0 +1,13 @@
+//"use client";
+
+async function TasksPage() {
+  const response = await fetch("http://localhost:3000/api/tasks", {
+    cache: "no-store",
+  });
+  const tasks = await response.json();
+
+  console.log("tasks:", tasks);
+
+  return <div>TasksPage</div>;
+}
+export default TasksPage;
